49accc11c6cb410db423d3c0883ca8e210c34612
aribicki <aribicki@saintjoe.edu>
2015-02-10 23:20:48 -0600

    Create Dice_ALR.js
    first go at code

---
+++ Dice_ALR.js
@@ -0,0 +1,8 @@
+// Andrew Ribicki
+// CMP237 Dice Roller
+// 2/10/2015
+
+ var dice1 = Math.floor(Math.random() * 6) + 1
+	// for dice1 pick a random number between 1 and 6
+ var dice2 = Math.floor(Math.random() * 6) + 1
+	// for dice2 pick a random number between 1 and 6
