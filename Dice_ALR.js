a4dcfb70cb5aaaccfe6d56903656f92fe7e59170
Andrew <aribicki@gmail.com>
2015-02-11 00:31:31 -0500

    added new line to display the random dice rolls

--- Dice_ALR.js
+++ Dice_ALR.js
@@ -2,7 +2,10 @@
 // CMP237 Dice Roller
 // 2/10/2015
 
- var dice1 = Math.floor(Math.random() * 6) + 1
+ var dice1 = Math.floor(Math.random() * 6) + 1;
 	// for dice1 pick a random number between 1 and 6
- var dice2 = Math.floor(Math.random() * 6) + 1
+ var dice2 = Math.floor(Math.random() * 6) + 1;
 	// for dice2 pick a random number between 1 and 6
+ var dicenumbers = dice1,dice2;
+	// display the random numbers 
+
